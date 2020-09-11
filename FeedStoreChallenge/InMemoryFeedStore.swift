7a9d324fdcae4cb83527fc20467958c62ffad26c
Nikita <nsdidenko.developer@gmail.com>
2020-09-12 00:24:42 +0300

    Retrieve delivers found values on non empty cache

--- FeedStoreChallenge/InMemoryFeedStore.swift
+++ FeedStoreChallenge/InMemoryFeedStore.swift
@@ -5,16 +5,26 @@
 import Foundation
 
 public final class InMemoryFeedStore: FeedStore {
+    private var images = [LocalFeedImage]()
+    private var timestamp: Date?
+    
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         
     }
     
     public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        images.append(contentsOf: feed)
+        self.timestamp = timestamp
         
+        completion(nil)
     }
     
     public func retrieve(completion: @escaping RetrievalCompletion) {
-        completion(.empty)
+        if images.isEmpty {
+            completion(.empty)
+        } else {
+            completion(.found(feed: images, timestamp: timestamp ?? Date()))
+        }
     }
     
     public init() {}
