62535981617510058bd869ef90ca4495952f42a1
Nikita <nsdidenko.developer@gmail.com>
2020-09-12 01:11:53 +0300

    Prepare the initial state of a CodableFeedStoreChallenge

---
+++ FeedStoreChallenge/CodableFeedStore/CodableFeedStore.swift
@@ -0,0 +1,19 @@
+//
+//  Copyright © 2020 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+public final class CodableFeedStore: FeedStore {
+    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        
+    }
+    
+    public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        
+    }
+    
+    public func retrieve(completion: @escaping RetrievalCompletion) {
+        
+    }
+}
