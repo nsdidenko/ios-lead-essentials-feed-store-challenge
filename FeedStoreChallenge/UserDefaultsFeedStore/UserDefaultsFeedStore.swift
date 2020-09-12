404d29ba2f33c1bb96e37aa04eb048790c730fdf
Nikita <nsdidenko.developer@gmail.com>
2020-09-12 14:41:47 +0300

    Create private 'UserDefaultsFeedImage' witch conforms 'Codable' instead of 'LocalFeedImage'

--- FeedStoreChallenge/UserDefaultsFeedStore/UserDefaultsFeedStore.swift
+++ FeedStoreChallenge/UserDefaultsFeedStore/UserDefaultsFeedStore.swift
@@ -5,11 +5,6 @@
 import Foundation
 
 public final class UserDefaultsFeedStore: FeedStore {
-    private struct Cache: Codable {
-        let images: [LocalFeedImage]
-        let timestamp: Date
-    }
-    
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         defaults.removeObject(forKey: key)
         
@@ -17,7 +12,7 @@ public final class UserDefaultsFeedStore: FeedStore {
     }
     
     public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-        let cache = Cache(images: feed, timestamp: timestamp)
+        let cache = Cache(feed: feed.map(UserDefaultsFeedImage.init), timestamp: timestamp)
         let feedData = try! JSONEncoder().encode(cache)
         defaults.set(feedData, forKey: key)
         
@@ -30,13 +25,42 @@ public final class UserDefaultsFeedStore: FeedStore {
         
         let cache = try! JSONDecoder().decode(Cache.self, from: feedData)
         
-        completion(.found(feed: cache.images, timestamp: cache.timestamp))
+        completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
     }
+    
+    public init(key: String) {
+        self.key = key
+    }
+    
+    // MARK: - Private
     
     private let defaults = UserDefaults.standard
     private let key: String
     
-    public init(key: String) {
-        self.key = key
+    private struct Cache: Codable {
+        let feed: [UserDefaultsFeedImage]
+        let timestamp: Date
+        
+        var localFeed: [LocalFeedImage] {
+            feed.map { $0.local }
+        }
+    }
+    
+    private struct UserDefaultsFeedImage: Codable {
+        private let id: UUID
+        private let description: String?
+        private let location: String?
+        private let url: URL
+        
+        init(image: LocalFeedImage) {
+            id = image.id
+            description = image.description
+            location = image.location
+            url = image.url
+        }
+        
+        var local: LocalFeedImage {
+            LocalFeedImage(id: self.id, description: self.description, location: self.location, url: self.url)
+        }
     }
 }
