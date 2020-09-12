e2a496330d72cbcbd906cc1f650784a7a23d5712
Nikita <nsdidenko.developer@gmail.com>
2020-09-12 16:11:28 +0300

    Add 'FileSystemFeedStore' without implementation and 'FileSystemFeedStoreChallengeTests' with commented tests

---
+++ FeedStoreChallenge/FileSystemFeedStore/FileSystemFeedStore.swift
@@ -0,0 +1,21 @@
+//
+//  Copyright © 2020 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+public final class FileSystemFeedStore: FeedStore {
+    public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        
+    }
+    
+    public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        
+    }
+    
+    public func retrieve(completion: @escaping RetrievalCompletion) {
+        
+    }
+    
+    public init() {}
+}
