6f4094edab76e1d9657b3e56dc788fcd1a419996
Nikita <nsdidenko.developer@gmail.com>
2020-09-12 12:07:32 +0300

    Delete delivers no error on non empty cache in UserDefaultsFeedStore

--- Tests/UserDefaultsFeedStore/UserDefaultsFeedStoreChallengeTests.swift
+++ Tests/UserDefaultsFeedStore/UserDefaultsFeedStoreChallengeTests.swift
@@ -78,9 +78,9 @@ class UserDefaultsFeedStoreChallengeTests: XCTestCase, FeedStoreSpecs {
     }
 
     func test_delete_deliversNoErrorOnNonEmptyCache() {
-//        let sut = makeSUT()
-//
-//        assertThatDeleteDeliversNoErrorOnNonEmptyCache(on: sut)
+        let sut = makeSUT()
+
+        assertThatDeleteDeliversNoErrorOnNonEmptyCache(on: sut)
     }
 
     func test_delete_emptiesPreviouslyInsertedCache() {
